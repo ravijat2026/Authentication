a6e143508ac6f47183935e768e264b5d15cfadc6
ravijat2026 <ravijat2026@gmail.com>
2024-10-17 01:31:21 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://authentication-two-weld.vercel.app",
     credentials: true,
 }))
 
