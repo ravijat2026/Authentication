a15dc351b8511a9df4d88762ac2c0d489ee987ae
ravijat2026 <ravijat2026@gmail.com>
2024-10-17 01:44:11 +0530

    App.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ const ProtectedRoute = ({ children }) => {
 		return <Navigate to='/login' replace />;
 	}
 
-	if (!user.isVerified) {
+	if (!user?.isVerified) {
 		return <Navigate to='/verify-email' replace />;
 	}
 
@@ -32,7 +32,7 @@ const ProtectedRoute = ({ children }) => {
 const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
 
-	if (isAuthenticated && user.isVerified) {
+	if (isAuthenticated && user?.isVerified ) {
 		return <Navigate to='/' replace />;
 	}
 
