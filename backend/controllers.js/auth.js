6b1e1455d82afd0693d3102b9abf3e0e8831d833
ravijat2026 <ravijat2026@gmail.com>
2024-10-16 11:42:53 +0530

    change file

--- backend/controllers.js/auth.js
+++ backend/controllers.js/auth.js
@@ -1,4 +1,4 @@
-import { User } from "../models/User.js";
+import { User } from "../models/user.js";
 import bcryptjs from 'bcryptjs';
 import crypto from 'crypto';
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
